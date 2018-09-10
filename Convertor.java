c50745199928e6e62d85269ea9d55823b174f9d6
Amambayeva <39824103+Amambayeva@users.noreply.github.com>
2018-09-10 23:50:07 +0600

    Add files via upload

---
+++ Convertor.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+    public class Convertor {
+        public static String[] small_numbers = {"", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve",
+                "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+                "eighteen", "nineteen"};
+        public static String[] large_numbers = {
+                "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+        };
+
+        public static String inWords(int number) {
+            if (number < 20) {
+                return small_numbers[number];
+            }
+            if (number < 100) {
+                return large_numbers[number / 10] + ((number % 10 != 0) ? " " : "") + small_numbers[number % 10];
+            }
+            if (number < 1000) {
+                return small_numbers[number / 100] + " hundred " + "[and]" + ((number % 100 != 0) ? " " : "") + inWords(number % 100);
+            }
+            if (number < 100000) {
+                return inWords(number / 1000) + " thousands" + ((number % 10000 != 0) ? " " : "") + inWords(number % 1000);
+            }
+            return null;
+        }
+
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            int input = sc.nextInt();
+            if (input == 0){
+                System.out.println("zero");
+            }
+            System.out.println(inWords(input));
+        }
+    }
