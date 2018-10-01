ad67e8b347b36e1f8865e32f6c520414e9322df7
miracle1698 <39824103+miracle1698@users.noreply.github.com>
2018-10-01 20:03:15 +0600

    Update King.java

--- Week 3/Lab 3/Bonus/src/King.java
+++ Week 3/Lab 3/Bonus/src/King.java
@@ -1,9 +1,7 @@
 public class King extends Piece {
     public static Position x;
     public static Color color;
-    public String black = "\u2654";
-    public String white = "\u265A";
-
+   
     public boolean isLegalMove(Position x, Position y) {
         if(y.letter == (char)((int)x.letter + 1) || y.letter == (char)((int)x.letter - 1)) {
             if(y.number == x.number || y.number == x.number + 1 || y.number == x.number - 1)
